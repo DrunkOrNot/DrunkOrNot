744f7569f9c6652403ad6a1c620d0cb6b2bc6477
keylook23 <zachara_damian@wp.pl>
2019-11-06 21:58:11 +0100

    Version 1.0.0

---
+++ app/src/main/java/com/zacharadamian/drunkornot/MainActivity.java
@@ -0,0 +1,54 @@
+package com.zacharadamian.drunkornot;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+    private DatabaseReference mDatabase;
+    TextView txtData;
+    Button btnGo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnGo = findViewById(R.id.btnGo);
+        txtData = this.findViewById(R.id.txtData);
+        btnGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mDatabase = FirebaseDatabase.getInstance().getReference().child("sensor");
+                Query last = mDatabase.orderByKey().limitToLast(1);
+                last.addValueEventListener(new ValueEventListener() {
+                    @SuppressLint("SetTextI18n")
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                            String result = (String) ds.getValue();
+                            txtData.setText(result);
+
+                        }
+                    }
+                    @SuppressLint("SetTextI18n")
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        txtData.setText("error");
+                    }
+                });
+            }
+        });
+    }
+}
