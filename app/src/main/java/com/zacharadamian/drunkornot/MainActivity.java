9a0156f530320a8a0480871699308fd81d55b378
keylook23 <zachara_damian@wp.pl>
2019-11-17 21:38:15 +0100

    if
    if

--- app/src/main/java/com/zacharadamian/drunkornot/MainActivity.java
+++ app/src/main/java/com/zacharadamian/drunkornot/MainActivity.java
@@ -41,7 +41,11 @@ public class MainActivity extends AppCompatActivity {
         spSex.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (spSex.getSelectedItem().equals("male")) {
 
+                }else{
+
+                }
             }
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
